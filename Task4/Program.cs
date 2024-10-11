801d9abe3179b8d60f9a00c9ea7eb7f11b6c9566
Valery <wal_wali4@mail.ru>
2024-10-11 23:38:45 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Answer {
+static void PrintOddNumbers(int number)
+{
+// Введите свое решение ниже
+int i = 1;
+
+while (i < number)
+{
+i = i + 2;
+
+Console.Write (i + " ");
+}
+} 
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 7;
+}
+PrintOddNumbers(number);
+}
+}
