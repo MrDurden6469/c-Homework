df0f03f250f5309e1e47b0e3d6c37ac9730b67cf
Valery <wal_wali4@mail.ru>
2024-10-11 23:22:41 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Answer {
+static void CheckNumber(int number)
+{
+if (number > 0)
+{
+Console.WriteLine("положительное");
+}
+else if (number < 0)
+{
+Console.WriteLine("отрицательное");
+}
+else
+{
+Console.WriteLine("ноль");
+}
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 7;
+}
+CheckNumber(number);
+}
+}
