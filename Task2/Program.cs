5f0cfeaa1676c3c8b286fd623bdbfe442d872841
Valery <wal_wali4@mail.ru>
2024-10-11 23:18:53 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Answer {
+static int FindMedian(int a, int b, int c)
+
+{
+if (a >= b && a <= c)
+{
+return a;
+}
+else if (a >= c && a <= b)
+{
+return a;    
+}
+else if (b >= a && b <= c) 
+{
+return b;
+}
+else if (b >= c && b <= a)
+{
+    return b;
+}
+else
+{
+return c;
+}
+}
+static public void Main(string[] args) {
+int a, b, c;
+if (args.Length >= 3) {
+a = int.Parse(args[0]);
+b = int.Parse(args[1]);
+c = int.Parse(args[2]);
+} else {
+a = 5;
+b = 3;
+c = 8;
+}
+int result = FindMedian(a, b, c);
+System.Console.WriteLine($"{result}");
+}
+}
