caefe7a335d228c15d689cab311d162113a2e9fa
Valery <wal_wali4@mail.ru>
2024-10-18 14:35:49 +0300

    Task3

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (number < 10 && number >99) 
+Console.WriteLine("Ошибка ввода");
+
+else 
+
+if (firstDigit > secondDigit)
+
+Console.WriteLine(firstDigit);
+
+else 
+
+Console.WriteLine(secondDigit);
