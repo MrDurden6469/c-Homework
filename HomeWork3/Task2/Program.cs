696b1187c3d19a5e27ffe6508c19677911893f19
Valery <wal_wali4@mail.ru>
2024-10-23 00:49:40 +0300

    Task2

---
+++ HomeWork3/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = new int [10]{-3,-5,5,6,-7,8,-9,1,3,4};
+
+for (int i =0; i < array.Length; i++)
+{
+    array[i] *= -1;
+}
+
+for (int i =0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
