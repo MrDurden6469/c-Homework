91c35eb1ef3798af72348d50137382809da429f5
Valery <wal_wali4@mail.ru>
2024-10-23 01:11:55 +0300

    Task3

---
+++ HomeWork3/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿double [] array = new double [10]{3,5,4,6,7,8,9,1,2,10};
+
+double sum = 0;
+
+for (int index = 0; index < array.Length; index++)
+{
+    sum = sum + array[index];
+}
+
+double result = sum / array.Length;
+Console.WriteLine(result);
