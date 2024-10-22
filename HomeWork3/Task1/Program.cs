e283be4f76adb669902e644a0e583bce7123a9b4
Valery <wal_wali4@mail.ru>
2024-10-23 00:29:25 +0300

    Task1

---
+++ HomeWork3/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {3, 7, 6, 8, 9};
+int num = 8;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+    isExistNum = true;
+    break;
+    }
+}
+//string result = isExistNum ? "Да" : "Нет";
+Console.Write(isExistNum ? "Присутствует" : "Не присутствует");
