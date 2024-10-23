d3c5ed50669ef7cf668c530975ab310e92b8c24e
Valery <wal_wali4@mail.ru>
2024-10-24 01:02:15 +0300

    Task4

---
+++ HomeWork3/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] array = {-3, 4, 5, -7, -5, 9};
+int index = 0;
+int count = 0;
+while (index != array.Length)
+{
+    if (0 <= array[index])
+   
+   
+    count++;
+    index++;
+
+}
+  int [] NewArray = new int[count];
+  while (index != array.Length)
+{
+    if (0 < array[index])
+   
+   count++;
+   index++;
+   NewArray[count] = array[index];
+   
+}
+Console.Write("[");
+for (int j = 0; j < NewArray.Length; j++)
+{
+    Console.Write($"{NewArray[j]}," );
+}
+Console.Write("]");
